aa2334bd4253437b3b5cef17ce62b5c22985c4de
tringuyenskymavis <tri.minh.nguyen@skymavis.com>
2024-08-18 23:06:02 +0700

    test

--- src/Counter.sol
+++ src/Counter.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     uint256 public number;
+    uint256 b;
+    uint256 a;
+    uint256 c;
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
